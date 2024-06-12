e119db580fe3202e9ea9920191d5ba0a81893a0d
Shashank MG <37444395+ShashankMG123@users.noreply.github.com>
2024-06-12 12:38:49 -0700

    Fix player 2 bug when colliding with powerup

--- ChasersGambit/Assets/Scripts/CollisionManager.cs
+++ ChasersGambit/Assets/Scripts/CollisionManager.cs
@@ -8,8 +8,11 @@ public class CollisionManager : MonoBehaviour
     public GameObject hunter;
     public Camera hunterCam;
 
-    public GameObject hunted;
-    public Camera huntedCam;
+    public GameObject hunted1;
+    public Camera huntedCam1;
+    
+    public GameObject hunted2;
+    public Camera huntedCam2;
 
     public GameObject exit;
     public GameManager gameManager;
@@ -31,22 +34,22 @@ public class CollisionManager : MonoBehaviour
 
     void Start()
     {
-        UpdatePlayerControl();
+        UpdatePlayerControl(hunted1);
     }
 
-    public void OnHuntedCollisionWithPowerUp(GameObject powerUp)
+    public void OnHuntedCollisionWithPowerUp(GameObject powerUp, GameObject collidingHunted)
     {
         // Log a message to the console
         Debug.Log("Hunted collided with a PowerUp!");
 
         // Start the coroutine to switch roles for 5 seconds
-        StartCoroutine(SwitchRolesForDuration(5.0f));
+        StartCoroutine(SwitchRolesForDuration(5.0f, collidingHunted));
 
         // Optionally, destroy the power-up after collision
         Destroy(powerUp);
     }
 
-    public void OnHuntedCollisionWithHunter(GameObject hunter)
+    public void OnHuntedCollisionWithHunter()
     {
         // Log a message to the console
         Debug.Log("Hunted collided with hunter");
@@ -55,7 +58,7 @@ public class CollisionManager : MonoBehaviour
         gameManager.LoseGame();
     }
 
-    public void OnHuntedCollisionWithExit(GameObject exit)
+    public void OnHuntedCollisionWithExit()
     {
         // Log a message to the console
         Debug.Log("Hunted collided with exit");
@@ -64,41 +67,62 @@ public class CollisionManager : MonoBehaviour
         gameManager.WinGame();
     }
 
-    void UpdatePlayerControl()
+    void UpdatePlayerControl(GameObject collidingHunted)
     {
         // Activate/Deactivate role controls and chase mechanic
         if (isHuntedActive)
         {
-            hunted.GetComponent<HuntedController>().enabled = true;
+            if (collidingHunted.CompareTag("Hunted1"))
+            {
+                hunted1.GetComponent<HuntedController>().enabled = true;
+            }
+            else
+            {
+                hunted2.GetComponent<HuntedController>().enabled = true;
+            }
             hunter.GetComponent<HunterController>().isChaseActive = true;
         }
         else
         {
-            hunted.GetComponent<HuntedController>().enabled = false;
+            if (collidingHunted.CompareTag("Hunted1"))
+            {
+                hunted1.GetComponent<HuntedController>().enabled = false;
+            }
+            else
+            {
+                hunted2.GetComponent<HuntedController>().enabled = false;
+            }
             hunter.GetComponent<HunterController>().isChaseActive = false;
         }
     }
 
-    void UpdateCameraState()
+    void UpdateCameraState(GameObject collidingHunted)
     {
         // Activate/deactivate cameras
-        huntedCam.gameObject.SetActive(isHuntedActive);
+        if (collidingHunted.CompareTag("Hunted1"))
+        {
+            huntedCam1.gameObject.SetActive(isHuntedActive);
+        }
+        else
+        {
+            huntedCam2.gameObject.SetActive(isHuntedActive);
+        }
         hunterCam.gameObject.SetActive(!isHuntedActive);
     }
 
-    IEnumerator SwitchRolesForDuration(float duration)
+    IEnumerator SwitchRolesForDuration(float duration, GameObject collidingHunted)
     {
         // Switch roles
         isHuntedActive = !isHuntedActive;
-        UpdatePlayerControl();
-        UpdateCameraState();
+        UpdatePlayerControl(collidingHunted);
+        UpdateCameraState(collidingHunted);
 
         // Wait for the specified duration
         yield return new WaitForSeconds(duration);
 
         // Revert the roles back
         isHuntedActive = !isHuntedActive;
-        UpdatePlayerControl();
-        UpdateCameraState();
+        UpdatePlayerControl(collidingHunted);
+        UpdateCameraState(collidingHunted);
     }
 }
