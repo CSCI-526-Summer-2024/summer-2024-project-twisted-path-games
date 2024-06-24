1e08a5804cc3b2256c60ba87552135aab496477f
rrmangum <rrmangum@gmail.com>
2024-06-23 22:50:05 -0700

    Adding hunter changes

--- ChasersGambit/Assets/Scripts/Controllers/HunterController.cs
+++ ChasersGambit/Assets/Scripts/Controllers/HunterController.cs
@@ -5,9 +5,16 @@ namespace Controllers
     public class HunterController : MonoBehaviour
     {
         public float speed;
+        public float paceSpeed;
         public float increasedSpeed;
         float horizontalInput;
         float verticalInput;
+
+        private Vector3 startLocation;
+        public Vector3 checkPoint1;
+        public Vector3 checkPoint2;
+        private float positionThreshold = 0.1f;
+        private Vector3 currentMovementTarget;
 
         // Hunter rigid body component
         private Rigidbody rb;
@@ -20,6 +27,7 @@ namespace Controllers
         public float thresholdDistance = 5.0f;
 
         private bool isCurrentlyChasing = false;
+
         void Start()
         {
             // Prevent rotation based on physics interactions
@@ -32,6 +40,9 @@ namespace Controllers
 
             capsuleCollider = GetComponent<CapsuleCollider>();
 
+            startLocation = transform.position;
+            currentMovementTarget = startLocation;
+
             if (isChaseActive)
             {
                 rb.isKinematic = true;
@@ -42,41 +53,104 @@ namespace Controllers
         {
             if (isChaseActive)
             {
-                agent.isStopped = false;
-                capsuleCollider.enabled = true;
+                HandleAgent();
+            }
+            else
+            {
+                HandlePlayerControl();
+            }
+         }
 
-                if (CheckHuntedProximity())
+        private void HandleAgent()
+        {
+            agent.isStopped = false;
+            capsuleCollider.enabled = true;
+
+            if (CheckHuntedProximity())
+            {
+                StartChase();
+            }
+            else if (GameState.DidAnyHuntedExit)
+            {
+                if (IsAtPosition(transform.position, startLocation))
                 {
-                    if (!isCurrentlyChasing)
-                    {
-                        isCurrentlyChasing = true;
-                        GameState.numAggro++;
-                        Debug.Log("Chase started!");
-                        if (GameState.firstChase == -1.0f)
-                        {
-                            GameState.firstChase = Time.time; // Record the start time of the first chase
-                        }
-                    }
-
-                    Debug.Log("Chasing!");
-                    agent.SetDestination(target.transform.position);
+                    HandlePatrolling();
                 }
                 else
                 {
-                    if (isCurrentlyChasing)
-                    {
-                        isCurrentlyChasing = false;
-                        Debug.Log("Chase ended!");
-                    }
+                    ReturnToStartLocation();
                 }
             }
             else
             {
-                agent.isStopped = true;
-                rb.isKinematic = false;
-                capsuleCollider.enabled = false;
-                GetInputs();
-                MoveHunter();
+                HandlePatrolling();
+            }
+        }
+
+        void HandlePlayerControl()
+        {
+            agent.isStopped = true;
+            rb.isKinematic = false;
+            capsuleCollider.enabled = false;
+            GetInputs();
+            MoveHunter();
+        }
+
+        void StartChase()
+        {
+            currentMovementTarget = target.transform.position;
+            if (!isCurrentlyChasing)
+            {
+                isCurrentlyChasing = true;
+                GameState.numAggro++;
+                Debug.Log("Chase started!");
+                if (GameState.firstChase == -1.0f)
+                {
+                    GameState.firstChase = Time.time; // Record the start time of the first chase
+                }
+            }
+
+            Debug.Log("Chasing!");
+            agent.speed = speed;
+            agent.SetDestination(target.transform.position);
+        }
+
+        void ReturnToStartLocation()
+        {
+            currentMovementTarget = startLocation;
+            agent.SetDestination(startLocation);
+        }
+
+        void HandlePatrolling()
+        {
+            if (isCurrentlyChasing)
+            {
+                isCurrentlyChasing = false;
+                Debug.Log("Chase ended!");
+            }
+            else
+            {
+                if (IsAtPosition(transform.position, currentMovementTarget))
+                {
+                    UpdatePatrolTarget();
+                }
+            }
+        }
+
+        void UpdatePatrolTarget()
+        {
+            agent.speed = paceSpeed;
+            if (currentMovementTarget == startLocation || currentMovementTarget == checkPoint2)
+            {
+                currentMovementTarget = checkPoint1;
+                agent.SetDestination(checkPoint1);
+                Debug.Log("Moving to checkPoint1");
+            }
+            else if (currentMovementTarget == checkPoint1)
+            {
+                currentMovementTarget = checkPoint2;
+                agent.SetDestination(checkPoint2);
+                Debug.Log("Moving to checkPoint2");
             }
         }
 
@@ -103,6 +177,11 @@ namespace Controllers
         {
             float distance = Vector3.Distance(target.transform.position, rb.transform.position);
             return distance <= thresholdDistance;
+        }
+
+        private bool IsAtPosition(Vector3 currentPos, Vector3 checkPoint)
+        {
+            return Vector3.Distance(currentPos, checkPoint) < positionThreshold;
         }
     }
 
